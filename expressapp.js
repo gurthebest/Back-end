4ab6cb4e1bca5f4cd9d9c80f59f4cbb3281ed24d
gurthebest <91569638+gurthebest@users.noreply.github.com>
2022-02-18 12:52:48 +0000

    Update expressapp.js

--- expressapp.js
+++ expressapp.js
@@ -92,6 +92,7 @@ app.use(function (req, res, next) {
         else next();
     });
 });
-
 const port = process.env.PORT ||3000
-app.listen(port)
+app.listen(port, function () {
+    console.log("App started on port 3000");
+})
