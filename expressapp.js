94f644c91497ca66ff8b75fc19abed5034534a92
gurthebest <91569638+gurthebest@users.noreply.github.com>
2022-02-18 15:58:56 +0000

    Update expressapp.js

--- expressapp.js
+++ expressapp.js
@@ -1,156 +1,172 @@
-const express = require('express')
-const app = express()
-
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var bodyParser = require('body-parser');
+var url = require("url");
 var path = require("path");
-var fs = require("fs");
-const { ObjectId } = require('mongodb');
 
-app.use(express.json())
+app.use(bodyParser());
+app.use(cors())
 
+    const MongoClient = require('mongodb').MongoClient;
+    let db;
+    MongoClient.connect('mongodb+srv://User2022:17102000Gur@cluster0.q3gsm.mongodb.net/testing', (err, client) => {
+        db = client.db('testing')
 
+    })
 
-const MongoClient = require('mongodb').MongoClient;
-
-
-
-
-let db
-MongoClient.connect('mongodb+srv://User2022:17102000Gur@cluster0.q3gsm.mongodb.net/testing', (err, client) => {
-    db = client.db('testing')
-
-})
+    //Allow CORS
     app.use(
         function
             (
                 req, res, next
             )
         {
-
+    // allow different IP address
             res.header(
                 "Access-Control-Allow-Origin"
                 ,
                 "*"
             );
+    // allow different header fields
             res.header(
                 "Access-Control-Allow-Headers"
                 ,
                 "*"
             ); next(); });
-app.use(function (req, res, next) {
-    console.log("Request IP: " + req.url);
-    console.log("Request date: " + new Date());
-    next(); // this should stop the browser from hanging
-});
-app.param('collectionName', (req, res, next, collectionName) => {
-    req.collection = db.collection(collectionName)
-    return next()
-})
+
+            // the 'logger' middleware
+            app.use(function(req, res, next) {
+                console.log("Request URL: " + req.url);
+                console.log("Request IP Address: " +  req.ip);
+                console.log("Request date: " + new Date());
+                next();
+            });
 
 
-app.get('/', (req, res, next) => {
-    res.send('Select a collection, e.g., /collection/messages')
-})
-
-
-app.get('/collection/:collectionName', (req, res, next) => {
-    req.collection.find({},)
-        .toArray((e, results) => {
-            if (e) return next(e)
-            res.send(results)
-        }
-        )
-})
-
-const ObjectID = require('mongodb').ObjectId;
-
-app.get('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.findOne(
-        { id: new ObjectID(req.params.id) },
-        (e, result) => {
-            if (e) return next(e)
-            res.send(result)
+        app.param('collectionName', (req, res, next, collectionName) => {
+            req.collection = db.collection(collectionName)
+          //  console.log(req.collection)
+            return next()
         })
-})
+
+
+        app.get('/collection/:collectionName', (req, res, next) => {
+            req.collection.find({}, {
+               // limit: 15,
+                sort: [
+                    ['price', -1]
+                ]
+            }).toArray((e, results) => {
+                if (e) return next(e)
+                res.send(results)
+            })
+            })
+
+        app.get("/Search/:collectionName/*", function(req, res) {
+            console.log("Performed a search query")
+            
+            var queries = url.parse(req.url, true).query;
+
+            var searchTerm = queries['search'];
+
+            if (searchTerm) {
+                console.log(searchTerm)
+               
+
+              if (!isNaN(searchTerm)){
+
+                  req.collection.find(
+                      { "id":  parseInt(searchTerm)}
+                  )
+                      .toArray((e, results) => {
+                          if (e) return next(e)
+                          res.send(results)
+                      })
+              } else {
+                  console.log("number is NOT integer")
+                  var regexx = new RegExp('^' + searchTerm );
+
+                  req.collection.find({ $or: [
+                          { "subject": { $regex: regexx}},
+                          { "location": { $regex: regexx }}
+                      ]})
+                      .toArray((e, results) => {
+                          
+                          if (e) return next(e)
+                          res.send(results)
+                      })
+              }
 
 
 
-app.post('/collection/:collectionName', (req, res, next) => {
-    req.collection.insert(req.body, (e, results) => {
-        if (e) return next(e)
-        res.send(results.ops)
-    })
-})
-app.put('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.updateOne(
-        { _id: new ObjectId(req.params.id) },
-        { $set: req.body },
-        { safe: true, multi: false },
-        (e, result) => {
-            if (e) return next(e)
-            res.send((result.update === 1) ?
-                { msg: 'error' } : { msg: 'success' })
-        })
-})
-app.use(function (req, res, next) {
-    // Uses path.join to find the path where the file should be
-    var filePath = path.join(__dirname, "images", req.url);
-    // Built-in fs.stat gets info about a file
-    fs.stat(filePath, function (err, fileInfo) {
-
-        if (err) {
-            if (err.code === 'ENOENT') {
-                res.statusCode = 404;
-                res.end('File not found')
             }
 
-            next();
-            return;
-        }
-        if (fileInfo.isFile()) res.sendFile(filePath);
-        else next();
-    });
-});
-app.get("/search/:collectionName/*", function(req, res) {
-    console.log("Search function ...")
+        });
 
-    var urlo = url.parse(req.url, true).query;
-    var searchWord = queries['search'];
 
-    if (searchWord) {
-        console.log(searchWord)
-
-      if (!isNaN(searchWord)){
-
-          req.collection.find(
-              { "id":  parseInt(searchWord)}
-          )
-              .toArray((e, results) => {
-
-                if (e) return next(e)
-                  res.send(results)
-              })
-      } else {
-          console.log("the number is't an intiger")
+        app.post('/collection/:collectionName', (req, res, next) => {
           
-          var regexx = new RegExp('^' + searchWord );
+            console.log("Received POST Request")
 
-          req.collection.find({ $or: [
-                  { "subject": { $regex: regexx}},
-                  { "location": { $regex: regexx }}
-              ]})
-              .toArray((e, results) => {
-                
-                  if (e) return next(e)
-                  res.send(results)
-              })
-      }
+            req.collection.insert(req.body, (e, results) => {
+                if (e) {
+                    return next(e)
+                }
+               
+            })
+    })
+
+    const ObjectID = require('mongodb').ObjectID;
+    app.put('/collection/:collectionName/:id', (req, res, next) => {
+        req.collection.updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $set: req.body },
+            { safe: true, multi: false },
+            (e, result) => {
+                if (e) return next(e)
+                res.send((result.update === 1) ?
+                    { msg: 'error' } : { msg: 'success' })
+            })
+    })
+        app.put('/update/:collectionName/:id', (req, res, next) => {
+            console.log("PUT REQUEST")
+            req.collection.update({
+                id  : parseInt(req.params.id)
+        }, {
+            $set: req.body
+        }, {
+            safe: true,
+            multi: false
+        }, (e, result) => {
+            console.log(result)
+            if (e) {
+
+                return next(e)
+            }
+            res.send((result.matchedCount === 1) ? {
+                msg: 'success'
+            } : {
+                msg: 'error'
+            })
+        })
+    })
+
+        // Sets up the path where your static files are
+        var publicPath = path.resolve(__dirname,  "assets");
+        // Sends static files from the publicPath directory
+        app.use(express.static(publicPath));
+        app.use(function(request, response) {
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.end("Picture Not Found.");
+        });
 
 
 
-    }
+    const port = process.env.PORT || 3000;
+    app.listen(port,function (){
 
-});
-const port = process.env.PORT ||3000
-app.listen(port, function () {
-    console.log("App started on port "+port);
-})
+        console.log("App started");
+    })
